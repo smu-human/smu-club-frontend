687cdb50a6efdd7fbe8035622089209f356a40da
LUVIZU <lypyin3@gmail.com>
2026-08-24 05:36:32 +0900

    fix

--- src/pages/club/club.tsx
+++ src/pages/club/club.tsx
@@ -12,7 +12,7 @@ import {
 } from "../../lib/api";
 import { Club } from "../../lib/types";
 
-const CANVAS_WIDTH = 340;
+const CANVAS_WIDTH = 320;
 const MIN_SCALE = 0.3;
 
 function useScaledCanvas() {
